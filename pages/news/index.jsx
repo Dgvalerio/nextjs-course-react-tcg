1e1f9d928c645261686fe11996961377528a397c
Davi Gonçalves Valério <al.davigvalerio@gmail.com>
2021-07-23 00:30:40 -0300

    Class 324 - Linking Between Pages

--- pages/news/index.jsx
+++ pages/news/index.jsx
@@ -1,3 +1,17 @@
-const NewsPage = () => <h1>The News Page</h1>;
+import Link from 'next/link';
+
+const NewsPage = () => (
+  <>
+    <h1>The News Page</h1>
+    <ul>
+      <li>
+        <Link href="/news/nextjs-is-a-great-framework">
+          NextJS Is A Great Framework
+        </Link>
+      </li>
+      <li>Something Else</li>
+    </ul>
+  </>
+);
 
 export default NewsPage;
